a333583b36eddf7a2458099b9ba314196d5551a7
vlados812 <vladislavolcev@gmail.com>
2026-05-11 23:03:31 +0300

    second commit

--- index.php
+++ index.php
@@ -1,33 +1,60 @@
 <?php
 
-$htmlContent = file_get_contents('index.html');
+class MetaTagIterator implements Iterator {
+    private $tags = [];
+    private $position = 0;
 
-
-$dom = new DOMDocument();
-@$dom->loadHTML($htmlContent);
-
-$data = [];
-
-
-$titleTags = $dom->getElementsByTagName('title');
-if ($titleTags->length > 0) {
-    $data['title'] = $titleTags->item(0)->nodeValue;
-}
-
-
-$metaTags = $dom->getElementsByTagName('meta');
-
-foreach ($metaTags as $tag) {
- 
-    if ($tag instanceof DOMElement) {
-        $name = strtolower($tag->getAttribute('name'));
+    public function __construct(string $htmlPath) {
+        $dom = new DOMDocument();
+        @$dom->loadHTMLFile($htmlPath);
         
-        if ($name == 'description' || $name == 'keywords') {
-            $data[$name] = $tag->getAttribute('content');
+        $this->extractTags($dom);
+    }
+
+    private function extractTags(DOMDocument $dom) {
+        $titleNodes = $dom->getElementsByTagName('title');
+        if ($titleNodes->length > 0) {
+            $this->tags['title'] = $titleNodes->item(0)->nodeValue;
         }
+        $metas = $dom->getElementsByTagName('meta');
+        foreach ($metas as $meta) {
+            if ($meta instanceof DOMElement) {
+                $name = strtolower($meta->getAttribute('name'));
+                if (in_array($name, ['description', 'keywords'])) {
+                    $this->tags[$name] = $meta->getAttribute('content');
+                }
+            }
+        }
+        
+
+        $this->keys = array_keys($this->tags);
+    }
+
+    public function rewind(): void {
+        $this->position = 0;
+    }
+
+    public function current(): mixed {
+        $key = $this->keys[$this->position];
+        return $this->tags[$key];
+    }
+
+    public function key(): mixed {
+        return $this->keys[$this->position];
+    }
+
+    public function next(): void {
+        ++$this->position;
+    }
+
+    public function valid(): bool {
+        return isset($this->keys[$this->position]);
     }
 }
 
+// Использование:
+$metaIterator = new MetaTagIterator('index.html');
 
-print_r($data);
-?>
+foreach ($metaIterator as $name => $content) {
+    echo "<b>$name</b>: $content <br>";
+}
